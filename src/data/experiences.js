e1f934f3eb9e676b09fa6679bfe5b87f24bb32a0
Shriniwas Dhage <dhageshriniwas@gmail.com>
2026-01-01 17:28:57 +0530

    Add files via upload

---
+++ src/data/experiences.js
@@ -0,0 +1,37 @@
+import genio from "../assets/images/genio.jpeg";
+import draic from "../assets/images/image2.png";
+import rbl from "../assets/images/RBL.png";
+import stealth from "../assets/images/stealth.jpeg";
+
+import { blue, green, pink, yellow } from "../utils";
+
+export const experiences = [
+  {
+    logo: stealth,
+    name: "Software Engineer Intern Certificate(February 2024)",
+    joined: "Feb'24",
+    end: "May'24",
+    title: "Software Engineer Intern",
+    bio: "Developed expertise in building full-stack web applications, integrating RESTful APIs, and managing complex User interfaces with responsive design.",
+    color: blue,
+  },
+  
+  {
+    logo: genio,
+    name: "GenioBits",
+    joined: "Dec'21",
+    end: "Mar'22",
+    title: "Full Stack Developer",
+    bio: "Worked on various projects from basic static sites to dynamic complex websites using ReactJS, Built Rest API's for various use cases",
+    color: yellow,
+  },
+  {
+    logo: draic,
+    name: "Posture Detector using Deep Learning",
+    joined: "Mar'24",
+    end: "June'25",
+    title: "Team Leader",
+    bio: "In Pimpri Chinchwad college Of Engineering we developed collaborative skills. Built amazing projects and brainstormed on various problem statements",
+    color: pink,
+  },
+];
---
+++ src/data/projects.js
@@ -0,0 +1,38 @@
+import stonks from "../assets/images/stonks.png";
+import sendfrom from "../assets/images/sendform.jpg";
+import semicolon from "../assets/images/semicolon.png";
+import scroll from "../assets/images/scroll.jpg";
+import getv from "../assets/images/get-v.jpg";
+
+export const projects = [
+  {
+    title: "weather forecasting app",
+    image: getv,
+    link: "https://dhageshriniwas-collab.github.io/weather-forecasting-app/",
+    description:
+      "A weather forecasting app provides real-time weather updates.",
+  },
+  {
+    title: "Gemini-clone",
+    image: semicolon,
+    link: "https://gemini-app1-eosin.vercel.app/",
+    description:
+      "chatbot for an AI assistant.",
+  },
+  {
+    title: "The Quiz App",
+    image: sendfrom,
+    link: "https://quiz-application-beta-orcin.vercel.app/",
+    description:
+      "Quiz application that tests your knowledge on various topics.",
+  },
+  {
+    title: "Interactive Travel Exploration Platform",
+    image: getv,
+    link: "https://tour-india-eight.vercel.app/",
+    description:
+      "A website for exploring travel destinations.",
+  },
+  
+  
+];
